f956c3b5b8d98d180c633f3b73c0c9f81935e55c
Tomasz Flis <tomaszwwl@gmail.com>
2024-10-29 21:19:32 +0100

    init

--- app/layout.tsx
+++ app/layout.tsx
@@ -28,6 +28,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+      {/*TODO: introduce navigation*/}
         {children}
       </body>
     </html>
